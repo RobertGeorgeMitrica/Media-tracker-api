d55ba935b88308d6f3b8e462a6573323057e5f4c
Robert <mitricarobert10@gmail.com>
2026-04-01 14:39:25 +0300

    Initial commit: Setup MediaTracker API with Spring Boot

---
+++ MediaTracker/src/main/java/com/example/MediaTracker/MediaTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.MediaTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaTrackerApplication.class, args);
+	}
+
+}
